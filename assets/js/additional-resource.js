afe5b573afa37794accc85cdae148d83f1361938
Zain Hassan <zainhassan927@gmail.com>
2024-06-26 18:39:57 +0500

    implement tab icon rotation on click

--- assets/js/additional-resource.js
+++ assets/js/additional-resource.js
@@ -3,11 +3,14 @@ const tabs = document.querySelectorAll(".tab .tab-button")
 tabs.forEach(tab => {
     tab.addEventListener("click", (e) => {
         const content = tab.parentElement.querySelector(".content");
+        const icon = tab.querySelector(".dark-fill");
 
         if(content.classList.contains("d-none")){
             content.classList.remove("d-none")
+            icon.classList.add("rotate")
         }else{
             content.classList.add("d-none")
+            icon.classList.remove("rotate")
         }
     })
 })
